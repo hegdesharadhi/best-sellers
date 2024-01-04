c726581f8d17c7b402f72d7feea991d0b970afea
Sharadhi Hegde <hegdesharadhi@gmail.com>
2024-01-04 10:32:53 +0530

    Changes to be pushed

---
+++ src/books.js
@@ -0,0 +1,23 @@
+import img1 from './images/atomic_habits.jpg'
+import img2 from './images/book2.jpg'
+import img3 from './images/book3.jpg'
+export const books = [
+  {
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    img: img1,
+    id: 1,
+  },
+  {
+    title: 'Ikigai',
+    author: 'Francesc Miralles',
+    id: 2,
+    img: img2,
+  },
+  {
+    title: "Don't Believe Everything You Think",
+    author: 'Joseph Nguyen',
+    id: 3,
+    img: img3,
+  },
+]
